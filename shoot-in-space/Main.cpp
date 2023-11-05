b4055e2b9df349a11bfed14ac75379e4e207cfb5
bellavee <bella.vnpv@gmail.com>
2023-11-05 13:34:03 +0100

    use code in chapter 15

--- shoot-in-space/Main.cpp
+++ shoot-in-space/Main.cpp
@@ -1,10 +1,10 @@
 
-#include "Graphics/Game.h"
-#include "Utils/pch.h"
-#include "Window/Window.h"
+// #include "Graphics/Game.h"
+// #include "Utils/pch.h"
+// #include "Window/Window.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
-{
-    Game game(1600, 900, L"Shoot-in-space");
-    return Window::Run(&game, hInstance, nCmdShow);
-}
+// int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+// {
+//     Game game(1600, 900, L"Shoot-in-space");
+//     return Window::Run(&game, hInstance, nCmdShow);
+// }
